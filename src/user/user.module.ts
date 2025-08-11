f8b9a54b713ebb34770d54ca355cd967f90e6e8e
jungyoubin <youbin@coconutsilo.com>
2025-08-11 13:57:51 +0900

    feat: registerAsync중복 수정

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,24 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from './auth/auth.module';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    ConfigModule, // ConfigModule 추가
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('jwt.secret') || 'secretKey',
-        signOptions: { expiresIn: '15m' },
-      }),
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
