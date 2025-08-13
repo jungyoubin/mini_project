1a9a5d4427faba0e2dd85c464746efed9f9194dd
jungyoubin <youbin@coconutsilo.com>
2025-08-13 09:54:52 +0900

    feat: SocketIO기반 Guard 설정하기

--- src/chat/guards/ws-jwt.guard.ts
+++ src/chat/guards/ws-jwt.guard.ts
@@ -1,28 +1,28 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Socket } from 'socket.io';
 
 @Injectable()
+// AuthGuard('jwt')를 WebSocket에 맞추어서 확장한 Guard -> WsJwtGuard
 export class WsJwtGuard extends (AuthGuard('jwt') as { new (): CanActivate }) {
-  // WS 컨텍스트용으로 Request 대체
   getRequest(context: ExecutionContext) {
+    // socket.io client 가져오기
     const client = context.switchToWs().getClient<Socket>();
-    // 1) socket.auth.token  2) Authorization 헤더(Bearer ...)
+    // handshake시 보낸 토큰 가져오기
+    // auth.token 또는 header 에서 꺼냄 -> auth.token(header는 혹시 모르니까 두기)
     const raw =
       (client.handshake as any)?.auth?.token || client.handshake.headers?.authorization || '';
-
-    // passport-jwt가 읽을 수 있도록 header 형식으로 만들어 반환
+    // bearere 없으면 앞에 붙이기
     const authorization =
       typeof raw === 'string' && !raw.toLowerCase().startsWith('bearer ') ? `Bearer ${raw}` : raw;
-
     return { headers: { authorization } } as any;
   }
 
   handleRequest(err: any, user: any, _info: any, context: ExecutionContext) {
+    // user에 payload 들어감
     if (err || !user) throw err || new UnauthorizedException();
-    // 통과 시 소켓에 user를 심어두면 게이트웨이에서 바로 사용 가능
     const client = context.switchToWs().getClient<Socket>();
-    (client as any).user = user;
+    (client as any).user = user; // socket 객체에 user 정보를 저장 -> gateway에서 사용가능
     return user;
   }
 }
