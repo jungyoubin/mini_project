154027be6cff2abd5cbdceed6c5b99bc2af33af5
jungyoubin <youbin@coconutsilo.com>
2025-08-13 09:12:38 +0900

    feat: Mongodb main.ts에 추가

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
   // ConfigService 가져오기
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port') ?? 3000;
+
+  mongoose.set('debug', true);
 
   await app.listen(port);
 }
