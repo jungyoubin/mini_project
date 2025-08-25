aa8723f3c80c26bc07f7adcb97b356a7373ae23e
jungyoubin <youbin@coconutsilo.com>
2025-08-25 15:34:21 +0900

    기본 세팅 및 파일 추가

---
+++ src/config/configuration.ts
@@ -0,0 +1,26 @@
+export default () => ({
+  port: parseInt(process.env.PORT ?? '3000', 10),
+
+  database: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT ?? '3306', 10),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    name: process.env.DB_NAME,
+  },
+
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    accessTTL: process.env.JWT_ACCESS_TTL ?? '1h',
+    refreshTTL: process.env.JWT_REFRESH_TTL ?? '7d',
+  },
+
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT ?? '6379', 10),
+  },
+  mongo: {
+    uri: process.env.MONGO_DB_CONNECT,
+    dbName: process.env.MONGO_DB_NAME || 'project',
+  },
+});
