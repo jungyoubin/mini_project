d1c602531e0e75f8641ac41b7e1d0e72bdc22168
jungyoubin <youbin@coconutsilo.com>
2025-08-26 12:27:20 +0900

    fix: 스키마 집합 구조에 대해 처리 방안

--- src/board/schemas/board.schema.ts
+++ src/board/schemas/board.schema.ts
@@ -24,11 +24,41 @@ export class Board {
   @Prop({ type: String, required: true }) // 작성자의 profile_id
   board_writer: string;
 
-  @Prop({ type: [{ profile_id: { type: String, required: true } }], default: [] })
+  /*
+  조사하였을때, 스키마에서는 집합 플래그는 따로 없으며,
+  좋아요를 추가 및 삭제를 진행할 때
+  $addToSet / $pull을 사용하여 중복 없이 처리를 진행하려고 합니다.
+  %addToSet 예시
+  
+  await this.boardModel.updateLike(
+    { board_id },
+    { $addToSet: { board_liked_people: { profile_id } } }
+  );
+
+  또는 합집합을 사용하여서 처리를 진행하려고 합니다.
+  합집합을 사용하면 해당 배열을 집합으로 취급하고 중복을 제거한다(순서x)
+  await this.boardModel.updateLike(
+    { board_id },
+    { $set: { board_liked_people: { $setUnion: [ "$board_liked_people", [ { profile_id } ] ] } } }
+  );
+  */
+
+  @Prop({
+    type: [
+      {
+        _id: false, // ← 서브도큐먼트 _id 생성 금지 (중복 방지)
+        profile_id: { type: String, required: true, trim: true },
+      },
+    ],
+    default: [],
+  })
   board_liked_people: { profile_id: string }[];
 
-  @Prop({ type: Number, default: 0 })
-  board_liked_count: number;
+  /* 
+  좋아요 갯수에 대해서는 따로 저장하지 않고 필요할 때,
+  다음과 같은 $size를 통하여서 집계하여서 사용하려고 합니다.
+  board_liked_count: { $size: "$board_liked_people" }
+  */
 }
 
 export const BoardSchema = SchemaFactory.createForClass(Board);
