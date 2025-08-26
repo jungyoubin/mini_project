adea6ef50198952ce02a46c73038cb8105bee849
jungyoubin <youbin@coconutsilo.com>
2025-08-26 14:57:59 +0900

    fix: DTO 네이밍케이스 변경

--- src/board/dto/create-board.dto.ts
+++ src/board/dto/create-board.dto.ts
@@ -3,9 +3,9 @@ import { IsString, MaxLength } from 'class-validator';
 export class CreateBoardDto {
   @IsString()
   @MaxLength(30, { message: '제목은 최대 30자입니다.' })
-  board_title: string;
+  boardTitle: string;
 
   @IsString()
   @MaxLength(200, { message: '본문은 최대 200자입니다.' })
-  board_content: string;
+  boardContent: string;
 }
