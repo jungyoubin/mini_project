4a8edd8b028435d6f64925528c807ddcbd116466
jungyoubin <youbin@coconutsilo.com>
2025-08-26 19:04:36 +0900

    fix: user.module에 jwt 호출 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,9 +5,22 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), JwtModule.register({}), RedisModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule, // 전역이면 생략 가능
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) => ({
+        secret: cfg.get<string>('jwt.secret'),
+        signOptions: { expiresIn: '15m' },
+      }),
+    }),
+    RedisModule,
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
