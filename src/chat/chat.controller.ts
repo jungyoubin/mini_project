a14af85fbb4864c713adb1521bda8e79c00488be
jungyoubin <youbin@coconutsilo.com>
2025-08-29 10:30:49 +0900

    feat: 채팅방 접속하기
    기존 참여자는 DB 추가하지 않음

---
+++ src/chat/chat.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  BadRequestException,
+  NotFoundException,
+  Param,
+  Get,
+} from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { CreateRoomDto } from './dto/create-room.dto';
+import { JwtAuthGuard } from '../common/guards/jwt.guard';
+import { ChatGateway } from './chat.gateway';
+import { ReqUser } from '../common/decorators/user.decorator';
+import { JwtPayloadDto } from 'src/common/payload/jwt-dto';
+
+@Controller('chat')
+export class ChatController {
+  constructor(
+    private readonly rooms: ChatService,
+    private readonly gateway: ChatGateway, // socket
+  ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('room/:roomId/join')
+  async join(@ReqUser() user: JwtPayloadDto, @Param('roomId') roomId: string) {
+    const profileId: string | undefined = user.sub;
+    if (!profileId) throw new BadRequestException('Invalid user payload');
+
+    // 방 있는지 확인하기
+    const room = await this.rooms.findRoomById(roomId);
+    if (!room) throw new NotFoundException('room not found');
+
+    // Participants 업데이트(있으면 패스)
+    const already = await this.rooms.isParticipant(roomId, profileId);
+    if (!already) {
+      await this.rooms.addParticipant(roomId, profileId);
+    }
+
+    //소켓 join
+    await this.gateway.joinProfileToRoom(profileId, roomId);
+
+    return {
+      room_id: roomId,
+      already_participant: already, // true면 기존 멤버였음, false는 신규 입장
+      joined: true, // 들어가졌는지 확인
+    };
+  }
+}
